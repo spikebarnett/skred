769dd28159d7d993185320520a39e97359060ddb
Chris Barnett <spike.barnett@gmail.com>
2014-09-26 12:57:58 -0400

    fixed directory bug

--- create.c
+++ create.c
@@ -6,18 +6,20 @@ int main(int argc, char **argv)
 {
   FILE *fp;
   int i,j;
-  for (i = 0; i < 1000; ++i)
+  for (i = 0; i < 2000; ++i)
   {
-    char dir[3];
+    char dir[255];
     sprintf(dir,"%i",i);
     mkdir(dir, 0777);
-    for (j = 0; j < 1000; ++j)
+    for (j = 0; j < 2000; ++j)
     {
-      char name[3],path[7];  
+      char name[255],path[512];  
       sprintf(name,"%i",j);
       strcpy(path,dir);
       strcat(path,"/");
       strcat(path,name);
+      
+      printf(path); printf("\n");
       
       fp = fopen(path, "w");
       fclose(fp);
--- skred.c
+++ skred.c
@@ -3,36 +3,16 @@
 #include <stdlib.h>
 #include <ftw.h>
 #include <unistd.h>
-#include <errno.h>
-#include <string.h>
 
 static int rm(const char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf)
 {
-  int res;
-  if (tflag == FTW_F) res = unlink(fpath);
-  if (tflag == FTW_D) res = rmdir(fpath);
-  if (res == -1)
-  {
-    printf(strerror(errno));
-    //printf(err); printf("\n");
-  }
+  if (tflag == FTW_F) unlink(fpath);
+  if (tflag == FTW_DP) rmdir(fpath);
   return 0;
 }
 
 int main (int argc, char **argv)
-{
-  char err[15];
-  sprintf(err,"%i", EACCES ); printf ("EACCES:  "); printf(err); printf("\n");
-  sprintf(err,"%i", EBUSY  ); printf ("EBUSY:   "); printf(err); printf("\n");
-  sprintf(err,"%i", EEXIST ); printf ("EEXIST:  "); printf(err); printf("\n");
-  sprintf(err,"%i", EINVAL ); printf ("EINVAL:  "); printf(err); printf("\n");
-  sprintf(err,"%i", EIO    ); printf ("EIO:     "); printf(err); printf("\n");
-  sprintf(err,"%i", ELOOP  ); printf ("ELOOP:   "); printf(err); printf("\n");
-  sprintf(err,"%i", ENOENT ); printf ("ENOENT:  "); printf(err); printf("\n");
-  sprintf(err,"%i", ENOTDIR); printf ("ENOTDIR: "); printf(err); printf("\n");
-  sprintf(err,"%i", EPERM  ); printf ("EPERM:   "); printf(err); printf("\n");
-  sprintf(err,"%i", EROFS  ); printf ("EROFS:   "); printf(err); printf("\n");
-  
+{ 
   if (nftw((argc < 2) ? "." : argv[1], rm, 20, FTW_DEPTH|FTW_PHYS) == -1)
   {
       perror("nftw");
